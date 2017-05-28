c87e1394ec1a7f5e39196000be5f6c76dba289b6
PalAditya <adityapal.nghss@gmail.com>
2017-05-28 16:12:08 +0530

    Update root.java

--- Stock Portal/src/stock/portal/root.java
+++ Stock Portal/src/stock/portal/root.java
@@ -17,14 +17,38 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.sql.*;
-import java.util.List;
-import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class root implements Initializable{
+public class root extends Student implements Initializable {
+    String sport="",brand="",model="",item="";
+    @FXML ComboBox issueSport;
+    @FXML ComboBox issueBrand;
+    @FXML ComboBox issueItem;
+    @FXML ComboBox issueModel;
+    @FXML TextField IssueRoll;
+    @FXML TextField issueQuantity;
+    @FXML DatePicker issueDate;
+    @FXML Button issueUpdate;
+    
+    @FXML ComboBox returnSport;
+    
+    @FXML ComboBox returnBrand;
+    @FXML ComboBox returnItem;
+    @FXML ComboBox returnModel;
+    @FXML TextField returnRoll;
+    @FXML TextField returnQuantity;
+    @FXML DatePicker returnDate;
+    @FXML Button returnUpdate;
     
     @FXML TextField studentEmail;
     @FXML TextField studentRoom;
@@ -48,19 +72,139 @@ public class root implements Initializable{
     @FXML TextField itemDate;
     @FXML TextField itemTax;
     @FXML Button itemSubmit;
-    @FXML Button itemDisplay;
-    @FXML TableView<Person> itemTable=new TableView<>();
-    ObservableList<Person> data=FXCollections.observableArrayList(new Person("A","B"));
+    
+    
+    @FXML ComboBox studentField;
+    @FXML TextField studentSearchT;
+    @FXML Button studentSearchB;
+    @FXML Button studentRefresh;
+    
+    @FXML ComboBox itemField;
+    @FXML TextField itemSearchT;
+    @FXML Button itemSearchB;
+    @FXML Button itemRefresh;
+    
+    @FXML TableView itemTable;
     @FXML TableView studentTable;
+
+    @FXML Tab studentRegister;
+    @FXML Tab studentDisplay;
+
+    @FXML Tab itemUpate;
+    @FXML Tab itemDisplay;
     
     Connection conn = null;
     
-     @FXML 
+    TableColumn<Student, String> colRoll =new TableColumn<Student, String>();
+    TableColumn<Student, String> colName =new TableColumn<Student, String>();
+    TableColumn<Student, String> colEmail =new TableColumn<Student, String>();
+    TableColumn<Student, String> colMobile =new TableColumn<Student, String>();
+    TableColumn<Student, String> colRoom =new TableColumn<Student, String>();
+    TableColumn<Student, String> colResidence =new TableColumn<Student, String>();
+    TableColumn<Student, String> colItemissued =new TableColumn<Student, String>();
+    TableColumn<Student, String> colQuality =new TableColumn<Student, String>();
+    TableColumn<Student, String> colIssuedate =new TableColumn<Student, String>();
+    TableColumn<Student, String> colReturndate =new TableColumn<Student, String>();
+    
+//    @FXML 
+//    private void showStudent(ActionEvent e) throws Exception
+//    {
+//        colRoll.setCellValueFactory(new PropertyValueFactory<Student,String>("Roll"));
+//        colName.setCellValueFactory(new PropertyValueFactory<Student,String>("Name"));
+//        colEmail.setCellValueFactory(new PropertyValueFactory<Student,String>("Email"));
+//        colMobile.setCellValueFactory(new PropertyValueFactory<Student,String>("Mobile"));
+//        colRoom.setCellValueFactory(new PropertyValueFactory<Student,String>("Room"));
+//        colResidence.setCellValueFactory(new PropertyValueFactory<Student,String>("Residence"));
+//        colItemissued.setCellValueFactory(new PropertyValueFactory<Student,String>("Itemissued"));
+//        colQuality.setCellValueFactory(new PropertyValueFactory<Student,String>("Quality"));
+//        colIssuedate.setCellValueFactory(new PropertyValueFactory<Student,String>("Isuedate"));
+//        colReturndate.setCellValueFactory(new PropertyValueFactory<Student,String>("Returndate"));
+//    
+//        studentTable.getColumns().addAll(colRoll,colName,colEmail,colMobile,colRoom,colResidence,colItemissued,colQuality,colIssuedate,colReturndate);
+//        studentTable.setItems(loadData());
+//        
+//        System.out.println("In table");
+//    }
+    
+    public ObservableList<Student> loadData(){
+        ObservableList<Student> data=FXCollections.observableArrayList();
+        
+        try{
+            connect();
+            String SQL="SELECT * FROM STUDENT";
+            ResultSet rs=conn.createStatement().executeQuery(SQL);
+            Student student=new Student();
+            while(rs.next()){
+                for(int i=0;i<=rs.getMetaData().getColumnCount();i++){
+                    student.setRoll(rs.getString("Roll Number"));
+                   // MessageBox.show(rs.getString("Roll Number"),"Error");
+                    student.setName(rs.getString("Name"));
+                    student.setEmail(rs.getString("Email Id"));
+                    student.setMobile(rs.getString("Mobile Number"));
+                    student.setRoom(rs.getString("Room Number"));
+                    student.setResidence(rs.getString("Residence"));
+                    //student.setItemissued(rs.getString("Issued Item"));
+                    //student.setQuality(rs.getString("Quality"));
+                    //student.setIssuedate(rs.getString("Issue Date"));
+                    //student.setReturndate(rs.getString("Return Date"));
+                    data.add(student);
+                }
+            }
+        }
+        catch(Exception e){
+            MessageBox.show(e.getMessage(),"Error");
+        }
+        
+        return data;
+    }
+    
+    @FXML 
+    private void onIssue(ActionEvent event)throws Exception
+    {
+        
+    }
+    
+    @FXML 
+    private void onReturn(ActionEvent event)throws Exception
+    {
+        boolean submit;
+        submit=ConfirmationBox.show("Are you sure that you want to submit?","Confirmation");
+        if(sport.equals("")||item.equals("")||model.equals("")||brand.equals(""))
+           System.out.println("Error");
+        else
+        {
+            try
+            {
+                connect();
+                String student=(String)returnRoll.getText();
+                String quantity=(String)returnQuantity.getText();
+                String sql="SELECT * FROM Stock WHERE Sport=? AND Item=? AND Model=? AND Brand=? AND IssuedTo=?";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setString(1,sport);
+                pstmt.setString(2,item);
+                pstmt.setString(1,model);
+                pstmt.setString(1,brand);
+                pstmt.setString(1,student);
+                ResultSet rs=pstmt.executeQuery();
+                String str=rs.getString("Issuedate");
+                //TODO: SomeCheckings with returnDate, possible fines
+                //TODO: Change Staus to Returned if all conditions are met?
+                
+            }
+            catch(Exception e)
+                    {
+                        System.out.println(e.getMessage());
+                    }
+        }
+            
+    }
+    
+    @FXML 
     private void onStudentUpdateClick(ActionEvent event)throws Exception
     {   
         boolean update;
         update=ConfirmationBox.show("Are you sure you want to update?","Confirmation");
-        System.out.println("Please show up");
+        
         if(update)
         {
             String name = studentName.getText();
@@ -109,30 +253,22 @@ public class root implements Initializable{
                 }
             }
         }
-    }
-    
-    public void connect()
-    {
-        try{
-            conn=DriverManager.getConnection("jdbc:sqlite:stock portal.sqlite"); 
-        }
-        catch(Exception e){
-            MessageBox.show(e.getMessage(),"Connection error");
-        }
-    }
-    
-    @FXML
-    private void onEquipmentDisplayClick(ActionEvent event)throws Exception
-    {
-        TableColumn one=new TableColumn("Brand");
-        one.setCellValueFactory(new PropertyValueFactory<>("Brand"));
-        TableColumn two=new TableColumn("Model");
-        two.setCellValueFactory(new PropertyValueFactory<>("Model"));
-        itemTable.setItems(data);
-        itemTable.getColumns().addAll(one,two);
-        data.add(new Person("Eh","Okay"));
-    }
+    }    
    
+    
+    
+    /*TabPane t=itemDisplay.getTabPane();
+   public void inside()
+   {
+       t.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
+
+  @Override
+  public void changed(ObservableValue<? extends Tab> arg0, Tab arg1, Tab arg2) {
+         System.out.println("Eqipment tab clicked");
+  }
+});
+       
+   }*/
     
     @FXML 
     private void onEquipmentUpdateClick(ActionEvent event)throws Exception
@@ -205,39 +341,112 @@ public class root implements Initializable{
             }
         }
     }
+   
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         studentResidence.getItems().addAll("MHR","SHR");
         studentSchool.getItems().addAll("SES","SBS","SMS","SIF","SMMMS");
-        
-        //itemTable.getColumns().addAll(/* edit and order these*/firstNameCol, lastNameCol, emailCol);
-        //studentTable.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
-        
+        returnSport.getItems().addAll("Cricket","Badminton","Volleyball","Tennis","Football","Basketball","3");
+        returnSport.setEditable(true);
+        returnBrand.setEditable(true);
+        returnModel.setEditable(true);
+        returnItem.setEditable(true);
+        //String sport=returnSport.getEditor().getText();
+        //returnBrand.getItems().addAll("Nike","Adidas");
+        //returnModel.setValue("Eh?");
+        colRoll.setCellValueFactory(new PropertyValueFactory<Student,String>("Roll"));
+        colName.setCellValueFactory(new PropertyValueFactory<Student,String>("Name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<Student,String>("Email"));
+        colMobile.setCellValueFactory(new PropertyValueFactory<Student,String>("Mobile"));
+        colRoom.setCellValueFactory(new PropertyValueFactory<Student,String>("Room"));
+        colResidence.setCellValueFactory(new PropertyValueFactory<Student,String>("Residence"));
+        colItemissued.setCellValueFactory(new PropertyValueFactory<Student,String>("Itemissued"));
+        colQuality.setCellValueFactory(new PropertyValueFactory<Student,String>("Quality"));
+        colIssuedate.setCellValueFactory(new PropertyValueFactory<Student,String>("Isuedate"));
+        colReturndate.setCellValueFactory(new PropertyValueFactory<Student,String>("Returndate"));
+    
+        studentTable.getColumns().addAll(colRoll,colName,colEmail,colMobile,colRoom,colResidence,colItemissued,colQuality,colIssuedate,colReturndate);
+        studentTable.setItems(loadData());
     }
-    public class Person
+    
+    public void connect()
     {
-        SimpleStringProperty Brand,Model;
-        Person(String a,String b)
-        {
-            this.Brand=new SimpleStringProperty(a);
-            this.Model=new SimpleStringProperty(b);
+        try{
+            conn=DriverManager.getConnection("jdbc:sqlite:stock portal.sqlite"); 
         }
-        public String getBrand()
-        {
-            return Brand.get();
+        catch(Exception e){
+            MessageBox.show(e.getMessage(),"Connection error");
         }
-        public String getModel()
+    }
+    public void returnSportItem()
+    {
+        sport=returnSport.getEditor().getText();
+        try
         {
-            return Model.get();
+            connect();
+            String sql="SELECT Brand FROM Stock WHERE Sport=?";
+            PreparedStatement pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1,sport);
+            ResultSet rs=pstmt.executeQuery();
+            while(rs.next())
+                returnBrand.getItems().addAll(rs.getString("Brand")+"");
+                //System.out.println(rs.getString("Brand"));
         }
-        public void setBrand(String a)
+        catch(Exception e)
         {
-            Brand.set(a);
+            System.out.println(e.getMessage());
         }
-        public void setModel(String b)
+    }
+    public void returnBrandItem()
+    {
+        String brand=returnBrand.getEditor().getText();
+        
+        try
         {
-            Model.set(b);
+            connect();
+            String sql="SELECT Model FROM Stock WHERE Sport=? AND Brand=?";
+            PreparedStatement pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1,sport);
+            pstmt.setString(2,brand);
+            ResultSet rs=pstmt.executeQuery();
+            while(rs.next())
+                returnModel.getItems().addAll(rs.getString("Model")+"");
+            returnModel.getItems().addAll("Okay");
+                //System.out.println(rs.getString("Brand"));
         }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void returnModelItem()
+    {
+        String model=returnModel.getEditor().getText();
+        
+        try
+        {
+            connect();
+            String sql="SELECT Item FROM Stock WHERE Sport=? AND Brand=? AND Model=?";
+            PreparedStatement pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1,sport);
+            pstmt.setString(2,brand);
+            pstmt.setString(3,model);
+            ResultSet rs=pstmt.executeQuery();
+            while(rs.next())
+                returnItem.getItems().addAll(rs.getString("Item")+"");
+                //System.out.println(rs.getString("Brand"));
+                returnItem.getItems().addAll("Okay");
+                
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void returnItemItem()
+    {
+        item=returnItem.getEditor().getText();
+        //returnItem.setValue("Okay");
     }
 }
